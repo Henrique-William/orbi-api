2fc1dd6b3ee52775eaa4d216df5a6044a78c842a
Henrique William <os.henriquewilliam@gmail.com>
2025-11-25 09:40:52 -0300

    refactor - Tabelas e relacionamentos ajustados para ter o id das rotas na coluna

---
+++ src/main/java/com/tech/orbi/entity/Package.java
@@ -0,0 +1,11 @@
+package com.tech.orbi.entity;
+
+public class Package {
+
+    private Integer id;
+    private String Address;
+    private String recipientName;
+    private String recipientPhone;
+    private String email;
+
+}
